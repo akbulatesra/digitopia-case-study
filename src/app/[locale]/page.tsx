ce5ac5779a70d5c5150629cf4d56fa2ca25c76df
esra akbulat <a.esra.akbulat@gmail.com>
2024-09-11 16:26:26 +0300

    every page turn to server

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,30 +1,12 @@
-'use client';
-import { Box, Typography } from '@mui/material';
-import Form from '../../components/Form';
-import { useTranslations } from 'next-intl';
-import { useEffect } from 'react';
-import { useAppSelector } from '@/redux/hook';
-import { useRouter } from 'next/navigation';
-import useResponsive from '@/hooks/useResponsive';
+import PageComponent from '@/components/Page';
+import { unstable_setRequestLocale } from 'next-intl/server';
 
-export default function Page() {
-  const { idToken } = useAppSelector((state) => state.user);
-  const router = useRouter();
-  const t = useTranslations('page');
-  const isSmDown = useResponsive('sm', 'down');
+export default function Page({
+  params: { locale },
+}: {
+  params: { locale: string };
+}) {
+  unstable_setRequestLocale(locale);
 
-  useEffect(() => {
-    idToken && router.push('/home');
-  }, [idToken, router]);
-
-  if (idToken) return null;
-
-  return (
-    <Box paddingX={isSmDown ? 2 : 10} paddingY={3}>
-      <Typography variant="h3" component="h1" align="center" mb={4}>
-        {t('welcome')}
-      </Typography>
-      <Form />
-    </Box>
-  );
+  return <PageComponent />;
 }
