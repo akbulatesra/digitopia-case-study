451666ab74952de182352fc68ab1afc8769f1cfb
esra akbulat <a.esra.akbulat@gmail.com>
2024-09-11 10:10:58 +0300

    EFIN-112 - useerrorlistener mak available for array of errors

--- src/hooks/useErrorListener.ts
+++ src/hooks/useErrorListener.ts
@@ -3,21 +3,41 @@ import { useEffect, useState } from 'react';
 const useErrorListener = (error: unknown) => {
   const [message, setMessage] = useState('');
   const [open, setOpen] = useState(false);
+
   useEffect(() => {
     if (error) {
-      switch (typeof error) {
-        case 'string':
-          setMessage(error);
-          break;
-        case 'object':
-          if ('data' in error && error) {
-            setMessage(error.data as string);
-          } else {
+      if (Array.isArray(error)) {
+        const combinedMessage = error
+          .map((err) => {
+            if (typeof err === 'string') {
+              return err;
+            } else if (
+              typeof err === 'object' &&
+              err !== null &&
+              'data' in err
+            ) {
+              return (err as { data: string }).data;
+            } else {
+              return 'An unexpected error occurred.';
+            }
+          })
+          .join(' | ');
+        setMessage(combinedMessage);
+      } else {
+        switch (typeof error) {
+          case 'string':
+            setMessage(error);
+            break;
+          case 'object':
+            if (error && 'data' in error) {
+              setMessage((error as { data: string }).data);
+            } else {
+              setMessage('An unexpected error occurred.');
+            }
+            break;
+          default:
             setMessage('An unexpected error occurred.');
-          }
-          break;
-        default:
-          setMessage('An unexpected error occurred.');
+        }
       }
       setOpen(true);
     } else {
@@ -31,4 +51,5 @@ const useErrorListener = (error: unknown) => {
 
   return { message, open, handleClose };
 };
+
 export default useErrorListener;
